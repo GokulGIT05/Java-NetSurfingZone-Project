41e6bb86f28cf03cfe798f903965d8b4b19d4337
gokul <gokul.g@aa.com>
2019-12-15 22:16:11 +0530

    Fibonacci

--- src/com/gk01To10/javaprograms/Gk04FibonacciNumber.java
+++ src/com/gk01To10/javaprograms/Gk04FibonacciNumber.java
@@ -15,9 +15,9 @@ public class Gk04FibonacciNumber {
 
 		System.out.println("The Fibonacci Numbers are: ");
 		// need to loop till the entered Value.
-		for (int i = 1; i <= inputValue; i++) {
-			 int fibNo = normalWay(i);
-		//	int fibNo = recursiveWay(i);
+		for (int i = 0; i <= inputValue; i++) {
+			int fibNo = normalWay(i);
+			// int fibNo = recursiveWay(i);
 			System.out.println(fibNo);
 		}
 
@@ -26,15 +26,17 @@ public class Gk04FibonacciNumber {
 
 	private static int normalWay(int input) {
 
+		if (input == 0) {
+			return 0;
+		}
 		if (input == 1 || input == 2) {
 			return 1;
 		}
 
-		
 		int first = 1;
 		int second = 1;
 		int fbNo = 1; // It can be 0 or 1.
-		
+
 		// 3 Index, we are using swap Function here,
 		for (int index = 3; index <= input; index++) {
 			fbNo = first + second;
@@ -47,13 +49,17 @@ public class Gk04FibonacciNumber {
 	// Am not getting how below works, so leave it.
 	private static int recursiveWay(int input) {
 
+		if(input<0) {
+			return -1;
+		}
+		
 		if (input == 1 || input == 2) {
 			System.out.println("inside: " + input);
 			return 1;
 		}
 		System.out.println("Outside " + input);
 		int first = recursiveWay(input - 1);
-		
+
 		System.out.println("End of First: " + first);
 		int Second = recursiveWay(input - 2);
 		System.out.println("End of Second: " + Second);
